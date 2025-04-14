3cc81c946753a26aca405e8c3d331ac3076c34d9
mdurant-bu <mdurant4@bu.edu>
2025-04-14 11:31:07 +0200

    Update runreadData.sh
    changed directories

--- PeroAna/runreadData.sh
+++ PeroAna/runreadData.sh
@@ -9,16 +9,18 @@ fi
 # Set the input directory and output directory
 #input_dir is the path where the raw data file (from the DAQ) are stored
 #input_dir="/Users/bordonis/ResearchActivities/Perovskite/data/NewSiPM_KEPboard_DPNCboard"
-input_dir="/Users/bordonis/ResearchActivities/Perovskite/data/NewSiPM_KEPboard_DPNCboard/CsPbBr3/Cs104-A" 
+#input_dir="/Users/bordonis/ResearchActivities/Perovskite/data/NewSiPM_KEPboard_DPNCboard/CsPbBr3/Cs104-A" 
 #input_dir="/Users/bordonis/ResearchActivities/Perovskite/data/NewSiPM_KEPboard_DPNCboard/MAPbBr3/SC67-B" 
 #input_dir="/Users/bordonis/ResearchActivities/Perovskite/data/NewSiPM_KEPboard_DPNCboard/CERN/MAPbBr3-SC67_B" 
 #input_dir="/eos/home-s/sbordoni/Perovskite/noiseTests/CERN/Cs104-A/"
+input_dir="/eos/home-m/midurant/Perovskite/data"
 
 #input_dir="/Users/bordonis/ResearchActivities/Perovskite/data/PCB_box/UniGe/BulkPerovskite"
 
 #rootfile_dir is the location where the rootfile built by the readData code is saved 
 #rootfile_dir="/afs/cern.ch/work/s/sbordoni/PerovskiteReD/PerovskiteAnalysis/PeroAna/rootinputfiles"
-rootfile_dir="/Users/bordonis/ResearchActivities/PerovskiteAnalysis/PeroAna/rootinputfiles"
+#rootfile_dir="/Users/bordonis/ResearchActivities/PerovskiteAnalysis/PeroAna/rootinputfiles"
+rootfile_dir="/eos/home-m/midurant/Perovskite/rootinputfiles"
 
 # Get the input file name from the command line argument
 input_file="$1"
